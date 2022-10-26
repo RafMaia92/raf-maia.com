6bcfd838e2d1f337e8aaed23452334f1f0ac247a
Rafael Maia <rafaelmaia@Rafaels-MacBook-Air.local>
2022-10-26 11:35:13 -0300

    fix: docs + root path

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  router: { base: '/' },
+  router: { base: '/raf-maia.com/' },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
