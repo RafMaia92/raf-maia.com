442bbeae7c34d2bc3a21298d5bc7e520899b560f
Rafael Maia <rafaelmaia@Rafaels-MacBook-Air.local>
2022-10-27 12:47:45 -0300

    2987fa3

---
+++ _nuxt/static/1666885641/blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1666885641",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6b954b6f"}}}}(null));
